90c9654bc1f4cf492254ce0e8d9f2dba712a68dc
Adil Hussain <adilson05uk@gmail.com>
2021-03-25 14:19:13 +0000

    Created an Xcode application project in the 'iOS' folder.

---
+++ iOS/AppCodeExperiment/AppCodeExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCodeExperimentApp.swift
+//  AppCodeExperiment
+//
+//  Created by Adil Hussain on 25/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppCodeExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
